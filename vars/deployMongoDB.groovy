af4e1407528f214c10d49d5c77cfaa55335de3c7
bhanu-dabas <160500397+bhanu-dabas@users.noreply.github.com>
2024-05-22 16:08:55 +0530

    Update deployMongoDB.groovy

--- vars/deployMongoDB.groovy
+++ vars/deployMongoDB.groovy
@@ -1,12 +1,12 @@
 def call() {
     // Clone the repository
-    git branch: 'main', url: 'https://github.com/bhanu-dabas/bhanusingh.git'
+    git branch: 'main', url: 'https://github.com/bhanu-dabas/mongo.git'
     // User Approval
     input message: 'Approve the deployment?', submitter: 'admin,bhanu-dabas'
     // Playbook Execution
     ansiblePlaybook(
-        inventory: 'hosts',
-        playbook: 'mongodb_install.yml'
+        inventory: 'aws_ec2.yml',
+        playbook: 'pb.yml'
     )
     // Notification
     emailext(
